9dca007fc5094c212f0f6a9ef297d144152ad596
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2016-03-12 15:02:51 +0100

    Implemented better heuristic.

--- metita/metita.py
+++ metita/metita.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python
 
+import argparse
 import sys
 
+import yaml
 
-MOVES = ((-3, 0), (-2, 2), (0, 3), (2, 2), (3, 0), (2, -2), (0, -3), (-2, -2))
+
+MOVES = yaml.load(open("moves.yml"))['moves']
+
+
+def _valid_moves(board, x, y):
+    moves = []
+
+    for move in MOVES:
+        _x = x + move[0]
+        _y = y + move[1]
+        if (_x >= 0 and _x < len(board) and _y >= 0 and _y < len(board[0]) and
+                board[_x][_y] < 1):
+            moves.append((_x, _y))
+
+    return moves
 
 
 def make_board(x, y):
-    return [[0] * y for _ in range(x)]
+    board = [[0] * y for _ in range(x)]
+
+    for i in range(x):
+        for j in range(y):
+            board[i][j] = -len(_valid_moves(board, i, j))
+
+    return board
 
 
 def _prioritise(board, moves):
-    weights = map(lambda x: min(x[0], len(board) - x[0] - 1) +
-        min(x[1], len(board[0]) - x[1] - 1), moves)
+    #weights = map(lambda x: min(x[0], len(board) - x[0] - 1) +
+    #    min(x[1], len(board[0]) - x[1] - 1), moves)
+    weights = map(lambda x: -board[x[0]][x[1]], moves)
+    print weights
     return zip(*sorted(zip(weights, moves)))[1]
 
 
-def metita(board, x, y, depth=1):
+def do_move(board, x, y, depth=1):
     """
     """
     board[x][y] = depth
@@ -24,24 +48,23 @@ def metita(board, x, y, depth=1):
     if depth == len(board) * len(board[0]):
         return True
 
-    _moves = []
-    for move in MOVES:
-        _x = x + move[0]
-        _y = y + move[1]
-        if (_x >= 0 and _x < len(board) and _y >= 0 and _y < len(board[0]) and
-                board[_x][_y] == 0):
-            _moves.append((_x, _y))
+    _moves = _valid_moves(board, x, y)
+    for move in _moves:
+        board[move[0]][move[1]] += 1
+    print_board(board)
 
     if _moves:
         for move in _prioritise(board, _moves):
-            if metita(board, move[0], move[1], depth + 1):
+            if do_move(board, move[0], move[1], depth + 1):
                 return True
 
-    board[x][y] = 0
+    board[x][y] = -1
+    for move in _moves:
+        board[move[0]][move[1]] -= 1
     return False
 
 
-def print_solution(board, handle=sys.stdout):
+def print_board(board, handle=sys.stdout):
     for row in board:
         for element in row:
             handle.write(' {:3d}'.format(element))
@@ -49,12 +72,16 @@ def print_solution(board, handle=sys.stdout):
     handle.write('\n')
 
 
+def metita(x, y, i, j):
+    board = make_board(x, y)
+    do_move(board, i, j)
+    print_board(board)
+
+
 def main():
     """
     """
-    board = make_board(10, 10)
-    metita(board, 0, 0)
-    print_solution(board)
+    metita(19, 19, 0, 0)
 
 
 if __name__ == '__main__':
